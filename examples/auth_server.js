26c7365667114a3df6e96a11ddd7e6f92e5c7869
Diego <dbordeu@uc.cl>
2018-12-26 12:09:03 -0300

    refactor

--- examples/auth_server.js
+++ examples/auth_server.js
@@ -1,72 +1,29 @@
 const radius = require('../lib/radius');
+const dgram = require('dgram');
 
 radius.add_dictionary('./examples/mikrotik.rfc2865');
-var dgram = require("dgram");
 
-var secret = 'herma123';
-var server = dgram.createSocket("udp4");
+const db = ['9C:4F:DA:11:56:34', '38:80:DF:13:4C:2E', '80:AD:16:E4:DA:DD', 'B8:5A:73:8A:CE:28'];
 
-server.on("message", async (msg, rinfo) => {
-  var code, username, password, packet;
-  packet = await readPacket(msg, secret);
+
+const secret = process.env.RADIUS_SECRET;
+const server = dgram.createSocket('udp4');
+
+server.on('message', async (msg, rinfo) => {
+  const packet = await readPacket(msg, secret);
+  const username = packet.attributes['User-Name'];
   console.log(packet, 'dsadsa');
 
-  if (packet.code == 'Accounting-Request') {
-
-    // console.log('------------------------' + packet.code + '----------------------');
-    code = 'Accounting-Response';
-    var response = radius.encode_response({
-      packet: packet,
-      code: code,
-      secret: secret
-    });
-    // console.log('Sending ' + code + ' for user ');
-    server.send(response, 0, response.length, rinfo.port, rinfo.address, function(err, bytes) {
-      if (err) {
-        console.log('Error sending response to ', rinfo);
-      }
-    });
-    return;
+  if (packet.code === 'Access-Request') {
+    console.log(`------------------------${packet.code}----------------------`);
+    checkAuth(username, packet, rinfo);
   }
-  if(packet.code == 'Access-Request') {
-    console.log('------------------------' + packet.code + '----------------------');
-    code = 'Access-Accept';
-    var response = radius.encode_response({
-      packet: packet,
-      code: code,
-      secret: secret
-    });
-
-
-    username = packet.attributes['User-Name'];
-
-    if(username !== '9C:4F:DA:11:56:34') {
-    // if(username !== '38:80:DF:13:4C:2E') {
-      code = 'Access-Reject';
-      response = radius.encode_response({
-        packet: packet,
-        code: code,
-        secret: secret
-      });
-    }
-
-    console.log('Sending ' + code + ' for user ' + username);
-    server.send(response, 0, response.length, rinfo.port, rinfo.address, function(err, bytes) {
-      if (err) {
-        console.log('Error sending response to ', rinfo);
-      }
-    });
-    return;
-  }
-  // console.log('------------------------' + packet.code + '----------------------');
-  // console.log('cagueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee');
-
 });
 
-server.on("listening", function () {
-  var address = server.address();
-  console.log("radius server listening " +
-      address.address + ":" + address.port);
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`radius server listening ${
+    address.address}:${address.port}`);
 });
 
 server.bind(1812);
@@ -74,9 +31,40 @@ server.bind(1812);
 function readPacket(msg, secret) {
   return new Promise((resolve, reject) => {
     try {
-      return resolve(radius.decode({packet: msg, secret: secret}));
+      return resolve(radius.decode({
+        packet: msg,
+        secret,
+      }));
     } catch (e) {
-      reject("Failed to decode radius packet, silently dropping:" + e);
+      reject(`Failed to decode radius packet, silently dropping:${e}`);
     }
   });
+}
+
+function checkAuth(username, packet, rinfo) {
+  if (db.includes(username)) {
+    console.log('siiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii');
+    sendAuth(true, username, packet, rinfo);
+  } else {
+    console.log('nooooooooooooooooooooooooooooooooooo');
+    sendAuth(false, username, packet, rinfo);
+  }
+}
+
+
+function sendAuth(auth, username, packet, rinfo) {
+  console.log(buildResponse(auth, packet), 'responseeeeeeeeeeeeee');
+  const response = radius.encode_response(buildResponse(auth, packet));
+  console.log(response.code);
+
+  console.log(`Sending ${response.code} for user ${username}`);
+  server.send(response, 0, response.length, rinfo.port, rinfo.address, (err) => {
+    if (err) {
+      console.log('Error sending response to ', rinfo);
+    }
+  });
+}
+
+function buildResponse(auth, packet) {
+  return auth ? { packet, code: 'Access-Accept', secret } : { packet, code: 'Access-Reject', secret };
 }
