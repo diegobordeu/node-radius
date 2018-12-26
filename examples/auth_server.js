1d1d6c04a1ccbda5df68113394714d34a36203b5
Diego <dbordeu@uc.cl>
2018-12-26 10:24:35 -0300

    refactor

--- examples/auth_server.js
+++ examples/auth_server.js
@@ -1,6 +1,4 @@
-// Example radius server doing authentication
-
-var radius = require('../lib/radius');
+const radius = require('../lib/radius');
 
 radius.add_dictionary('./examples/mikrotik.rfc2865');
 var dgram = require("dgram");
@@ -8,45 +6,61 @@ var dgram = require("dgram");
 var secret = 'herma123';
 var server = dgram.createSocket("udp4");
 
-server.on("message", function (msg, rinfo) {
-  console.log('mesaageeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee');
+server.on("message", async (msg, rinfo) => {
   var code, username, password, packet;
-  try {
-    packet = radius.decode({packet: msg, secret: secret});
-  } catch (e) {
-    console.log("Failed to decode radius packet, silently dropping:", e);
+  packet = await readPacket(msg, secret);
+  console.log(packet, 'dsadsa');
+
+  if (packet.code == 'Accounting-Request') {
+
+    // console.log('------------------------' + packet.code + '----------------------');
+    code = 'Accounting-Response';
+    var response = radius.encode_response({
+      packet: packet,
+      code: code,
+      secret: secret
+    });
+    // console.log('Sending ' + code + ' for user ');
+    server.send(response, 0, response.length, rinfo.port, rinfo.address, function(err, bytes) {
+      if (err) {
+        console.log('Error sending response to ', rinfo);
+      }
+    });
     return;
   }
-
-  if (packet.code != 'Access-Request') {
-    console.log('unknown packet type: ', packet.code);
-    return;
-  }
-
-  username = packet.attributes['User-Name'];
-  password = packet.attributes['User-Password'];
-
-  console.log('Access-Request for ' + username);
-  console.log('with password ' + password);
-
-  if (username == '80:AD:16:E4:DA:DD') {
+  if(packet.code == 'Access-Request') {
+    console.log('------------------------' + packet.code + '----------------------');
     code = 'Access-Accept';
-  }  else {
-    code = 'Access-Reject';
-  }
+    var response = radius.encode_response({
+      packet: packet,
+      code: code,
+      secret: secret
+    });
 
-  var response = radius.encode_response({
-    packet: packet,
-    code: code,
-    secret: secret
-  });
 
-  console.log('Sending ' + code + ' for user ' + username);
-  server.send(response, 0, response.length, rinfo.port, rinfo.address, function(err, bytes) {
-    if (err) {
-      console.log('Error sending response to ', rinfo);
+    username = packet.attributes['User-Name'];
+
+    if(username !== '9C:4F:DA:11:56:34') {
+    // if(username !== '38:80:DF:13:4C:2E') {
+      code = 'Access-Reject';
+      response = radius.encode_response({
+        packet: packet,
+        code: code,
+        secret: secret
+      });
     }
-  });
+
+    console.log('Sending ' + code + ' for user ' + username);
+    server.send(response, 0, response.length, rinfo.port, rinfo.address, function(err, bytes) {
+      if (err) {
+        console.log('Error sending response to ', rinfo);
+      }
+    });
+    return;
+  }
+  // console.log('------------------------' + packet.code + '----------------------');
+  // console.log('cagueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee');
+
 });
 
 server.on("listening", function () {
@@ -56,3 +70,13 @@ server.on("listening", function () {
 });
 
 server.bind(1812);
+
+function readPacket(msg, secret) {
+  return new Promise((resolve, reject) => {
+    try {
+      return resolve(radius.decode({packet: msg, secret: secret}));
+    } catch (e) {
+      reject("Failed to decode radius packet, silently dropping:" + e);
+    }
+  });
+}
